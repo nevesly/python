6f67b9cebe8a1060112247521ddab0909bb43364
nevesly <nevesly1109@gmail.com>
2014-12-17 16:56:19 +0800

    minor finetune for static files

--- bottle/web/wedding/program/index.py
+++ bottle/web/wedding/program/index.py
@@ -9,9 +9,15 @@ DEBUG = True
 
 app = Bottle()
 TEMPLATE_PATH.insert(0,'/cygdrive/e/code/git/python/bottle/web/wedding/template')
+STATIC='/cygdrive/e/code/git/python/bottle/web/wedding/static'
 
 @app.route("/")
 def index():
     return template("index.html", name = "fuck")
 
-run(app, host='localhost', port=8080, debug=DEBUG)
+# Static files
+@app.route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root=STATIC)
+
+run(app, host='0.0.0.0', port=8080, debug=DEBUG)
